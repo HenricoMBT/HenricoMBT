971f3459dba8a88cd6988703c063fc5002ccb933
Nischal <150942786+Nischalcodes@users.noreply.github.com>
2025-04-08 11:09:24 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,3 +12,15 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scrolled');
   }
 });
+
+
+
+const scrollArrow = document.getElementById("scrollArrow");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+        scrollArrow.classList.add("hidden");
+    } else {
+        scrollArrow.classList.remove("hidden");
+    }
+});
