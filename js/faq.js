4110eedeafc9a7ba706ec41336c994e0aef0ea54
Ishant <hcps-mekalais@henricostudents.org>
2025-04-16 22:34:22 -0400

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,17 +1,16 @@
 var acc = document.getElementsByClassName("accordion");
-      var i;
 
-      for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
-          this.classList.toggle("active");
-          this.parentElement.classList.toggle("active");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
 
-          var pannel = this.nextElementSibling;
+    var panel = this.nextElementSibling;
 
-          if (pannel.style.display === "block") {
-            pannel.style.display = "none";
-          } else {
-            pannel.style.display = "block";
-          }
-        });
+    if (panel.classList.contains("open")) {
+      panel.classList.remove("open");
+    } else {
+      panel.classList.add("open");
     }
+  });
+}
